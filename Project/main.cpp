7eeeb818466c961f1015aa642186eb6ca91d5dd6
Nils Höhner <nils.hoehner@googlemail.com>
2015-10-30 16:30:56 +0100

    added base project from AnimSim

---
+++ Project/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+#include "External/OpenGLLoader/gl_core_4_3.h"
+#include "External/GLFW/include/GLFW/glfw3.h"
+#include "External/GLM/glm/glm.hpp"
+#include "External/GLM/glm/gtc/matrix_transform.hpp"
+#include "External/picoPNG/picopng.h"
+
+// GLFW callback for errors
+static void errorCallback(int error, const char* description)
+{
+    std::cout << error << " " << description << std::endl;
+}
+
+// Main
+int main(void)
+{
+    int width = 800;
+    int height = 600;
+
+    // Initialize GLFW and OpenGL
+    GLFWwindow* pWindow;
+    glfwSetErrorCallback(errorCallback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    pWindow = glfwCreateWindow(width, height, "VoxelConeTracing", NULL, NULL);
+    if (!pWindow)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(pWindow);
+    ogl_LoadFunctions();
+
+    // OpenGL initialization
+    glDisable(GL_DEPTH_TEST);
+    glClearColor(0.0f, 0.0f, 0.0f, 1);
+    glEnable(GL_TEXTURE_1D);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_TEXTURE_3D);
+    glEnable(GL_CULL_FACE);
+
+    // Variables for the loop
+    GLfloat prevTime = (GLfloat)glfwGetTime();
+    GLfloat deltaTime;
+    GLint prevWidth = 0;
+    GLint prevHeight = 0;
+
+    glm::mat4 uniformView;
+    glm::mat4 uniformProjection = glm::perspective(glm::radians(35.0f), ((GLfloat)width / (GLfloat)height), 0.1f, 100.f);
+
+    // Loop
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        // Clear buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Get window resolution
+        GLint width, height;
+        glfwGetWindowSize(pWindow, &width, &height);
+
+        // Give OpenGL the window resolution
+        if (width != prevWidth || height != prevHeight)
+        {
+            glViewport(0, 0, width, height);
+            prevWidth = width;
+            prevHeight = height;
+        }
+
+        // Calc time per frame
+        GLfloat currentTime = (GLfloat)glfwGetTime();
+        deltaTime = currentTime - prevTime;
+        prevTime = currentTime;
+
+
+        // Prepare next frame
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+
+        std::cout << "\r" << "FPS: " << (int)(1.0f / deltaTime);
+    }
+
+    // Termination
+    glfwDestroyWindow(pWindow);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
