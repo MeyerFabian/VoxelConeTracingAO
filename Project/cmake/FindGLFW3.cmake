98491f2ef811a6d8159f0f53dcfc1ae8470bbd30
Nils Höhner <nils.hoehner@googlemail.com>
2015-12-06 17:49:20 +0100

    updated findglfw

--- Project/cmake/FindGLFW3.cmake
+++ Project/cmake/FindGLFW3.cmake
@@ -8,72 +8,91 @@
 #
 
 SET(GLFW3_SEARCH_PATHS
-	$ENV{GLFW3_ROOT}
-	${DEPENDENCIES_ROOT}
-	/usr			# APPLE
-	/usr/local		# APPLE
-	/opt/local		# APPLE
-	$ENV{PROGRAMFILES}/GLFW/
-	C:/GLFW
-        ${CMAKE_CURRENT_SOURCE_DIR}/externals/GLFW
-)
+        $ENV{GLFW3_ROOT}
+        ${DEPENDENCIES_ROOT}
+        /usr			# APPLE
+        /usr/local		# APPLE
+        /opt/local		# APPLE
+        )
 
-IF (MSVC)
-    FIND_PATH(GLFW_INCLUDE_DIRS
-        NAMES
-                GLFW/glfw3.h
-        PATHS
-                ${GLFW3_SEARCH_PATHS}
-        PATH_SUFFIXES
-                include
-        DOC
-                "The directory where GLFW/glfw3.h resides"
-    )
+IF (MINGW)
+    FIND_PATH(GLFW3_INCLUDE_PATH
+            NAMES
+            GLFW/glfw3.h
+            PATHS
+            ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+            include
+            DOC
+            "The directory where GLFW/glfw3.h resides"
+            )
 
-    IF(MSVC11)
-        set(SUFFIX "lib-vc2012")
-    ELSEIF(MSVC12)
-        set(SUFFIX "lib-vc2013")
-    ELSE()
-        set(SUFFIX "lib-vc2015")
-    ENDIF()
+    FIND_LIBRARY( GLFW3_LIBRARY
+            NAMES glfw3
+            PATHS
+            ${GLFW3_ROOT_ENV}/lib-mingw
+            )
 
-    FIND_PATH( GLFW_DLL
+ELSEIF (MSVC)
 
-        NAMES
-                glfw3.dll
-        PATHS
-                ${GLFW3_SEARCH_PATHS}
-        PATH_SUFFIXES
-                ${SUFFIX}
-        DOC
-                "The glfw3.dll library."
-	)
+    FIND_PATH(GLFW3_INCLUDE_PATH
+            NAMES
+            GLFW/glfw3.h
+            PATHS
+            ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+            include
+            DOC
+            "The directory where GLFW/glfw3.h resides"
+            )
+    FIND_LIBRARY(GLFW3_LIBRARY
+            NAMES
+            glfw3
+            PATHS
+            ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+            lib
+            DOC
+            "The directory where GLFW/glfw3.h resides"
+            )
 
-    FIND_LIBRARY( GLFW_LIBRARIES
+ELSEIF(APPLE)
 
-        NAMES
-                glfw3dll.lib
-        PATHS
-                ${GLFW3_SEARCH_PATHS}
-        PATH_SUFFIXES
-                ${SUFFIX}
-        DOC
-                "The glfw3dll.lib library."
-    )
+    FIND_PATH(GLFW3_INCLUDE_PATH
+            NAMES
+            GLFW/glfw3.h
+            PATHS
+            ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+            include
+            DOC
+            "The directory where GLFW/glfw3.h resides"
+            )
 
-else()
-    FIND_PATH(GLFW_INCLUDE_DIRS GLFW/glfw3.h)
-    FIND_LIBRARY(GLFW_LIBRARIES
-                NAMES glfw3 glfw
-                PATH_SUFFIXES dynamic)
+    FIND_LIBRARY(GLFW3_LIBRARY
+            NAMES
+            libglfw3.a glfw
+            PATHS
+            ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+            lib
+            DOC
+            "The directory where GLFW/glfw3.h resides"
+            )
+
+ELSE()
+    FIND_PATH(GLFW3_INCLUDE_PATH GLFW/glfw3.h)
+    FIND_LIBRARY(GLFW3_LIBRARY
+            NAMES glfw3 glfw
+            PATH_SUFFIXES dynamic)
 ENDIF ()
 
 
 SET(GLFW3_FOUND "NO")
-IF (GLFW_INCLUDE_DIRS AND GLFW_LIBRARIES AND GLFW_DLL)
-	SET(GLFW3_FOUND "YES")
+IF (GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
+    SET(GLFW3_LIBRARIES ${GLFW3_LIBRARY})
+    SET(GLFW3_FOUND "YES")
     message("EXTERNAL LIBRARY 'GLFW3' FOUND")
 ELSE()
     message("ERROR: EXTERNAL LIBRARY 'GLFW3' NOT FOUND")
-ENDIF (GLFW_INCLUDE_DIRS AND GLFW_LIBRARIES AND GLFW_DLL)
+ENDIF (GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
