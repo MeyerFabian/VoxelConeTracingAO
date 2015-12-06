57cb61656a1e1776377553cf00c96b87a23248b3
Nils Höhner <nils.hoehner@googlemail.com>
2015-12-06 17:41:23 +0100

    imported findASSIMP from cvark

--- Project/cmake/FindASSIMP.cmake
+++ Project/cmake/FindASSIMP.cmake
@@ -1,106 +1,71 @@
-# Based on the FindPhysFS.cmake scipt
-# - Try to find Assimp
-# Once done this will define
+# Copyright (C) 2014 Barry Deeney
+# Copyright (C) 2014 Benny Bobaganoosh
 #
-#  ASSIMP_FOUND - system has Assimp
-#  ASSIMP_INCLUDE_DIRS - the Assimp include directory
-#  ASSIMP_LIBRARIES - Link these to use Assimp
-#  ASSIMP_DLL
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-SET(ASSIMP_SEARCH_PATHS
-		$ENV{ASSIMP_ROOT}
-		${DEPENDENCIES_ROOT}
-		/usr			# APPLE
-		/usr/local		# APPLE
-		/opt/local		# APPLE
-		$ENV{PROGRAMFILES}/assimp/
-		C:/assimp
-		${CMAKE_CURRENT_SOURCE_DIR}/External/assimp
-		$ENV{ASSIMPSDIR}
-		/sw
-		/opt/csw
-		/opt
-		${_assimp_LIB_SEARCH_DIRS_SYSTEM}
+#############################################
+# Try to find ASSMIP and set the following: #
+#                                           #
+# ASSIMP_FOUND                              #
+# ASSIMP_INCLUDE_DIRS                       #
+# ASSIMP_LIBRARIES                          #
+#############################################
+
+SET( ASSIMP_SEARCH_PATHS
+    $ENV{ASSIMP_ROOT}                  # ASSIMP!
+    ${DEPENDENCIES_ROOT}
+    ./lib/Assimp
+    $ENV{PROGRAMFILES}/ASSIMP           # WINDOWS
+    ~/Library/Frameworks                # MAC
+    /Library/Frameworks                 # MAC
+    /usr/local                          # LINUX/MAC/UNIX
+    /usr                                # LINUX/MAC/UNIX
+    /opt                                # LINUX/MAC/UNIX
+    /sw                                 # Fink
+    /opt/local                          # DarwinPorts
+    /opt/csw                            # Blastwave
 )
 
-IF (MSVC)
-        FIND_PATH(ASSIMP_INCLUDE_DIRS
-                NAMES
-                        assimp/ai_assert.h
-                PATHS
-                        ${ASSIMP_SEARCH_PATHS}
-                PATH_SUFFIXES
-                        include
-                DOC
-                        "The directory where assimp/ai_assert.h resides"
-	)
+FIND_PATH( ASSIMP_INCLUDE_DIRS
+    NAMES
+        assimp/mesh.h
+    PATHS
+        ${ASSIMP_SEARCH_PATHS}
+    PATH_SUFFIXES
+        include
+    DOC
+        "The directory where assimp/mesh.h resides"
+)
 
-	FIND_LIBRARY(ASSIMP_LIBRARY_RELEASE
-		NAMES
-                        assimp.lib
-		PATHS
-                        ${ASSIMP_SEARCH_PATHS}
-		PATH_SUFFIXES
-			lib/Release
-		DOC
-                        "The assimp.lib library."
-	)
-	FIND_LIBRARY(ASSIMP_LIBRARY_DEBUG
-		NAMES
-			assimpd.lib
-		PATHS
-                        ${ASSIMP_SEARCH_PATHS}
-		PATH_SUFFIXES
-			lib/Debug
-		DOC
-                        "The assimpd.lib library."
-	)
+FIND_LIBRARY(ASSIMP_LIBRARIES
+    NAMES
+        assimp ASSIMP libassimp.a
+    PATHS
+        ${ASSIMP_SEARCH_PATHS}
+    PATH_SUFFIXES
+        lib
+        lib64
+        lib/x86
+        lib/x64
+    DOC
+        "The ASSIMP library"
+)
 
-	FIND_PATH( ASSIMP_DLL_DEBUG
-		NAMES
-			assimpd.dll
-		PATHS
-			${ASSIMP_SEARCH_PATHS}
-		PATH_SUFFIXES
-			bin/Debug
-		DOC
-			"The assimpd.dll library."
-	)
-
-	FIND_PATH( ASSIMP_DLL_RELEASE
-		NAMES
-			assimp.dll
-		PATHS
-			${ASSIMP_SEARCH_PATHS}
-		PATH_SUFFIXES
-			bin/Release
-		DOC
-			"The assimp.dll library."
-	)
-ENDIF ()
-
-
-SET(ASSIMP_FOUND "NO")
-IF (ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARY_DEBUG AND ASSIMP_LIBRARY_RELEASE AND ASSIMP_DLL_RELEASE AND ASSIMP_DLL_DEBUG)
-        SET(ASSIMP_FOUND "YES")
-        message("EXTERNAL LIBRARY 'ASSIMP' FOUND")
-ELSE()
-        message("ERROR: EXTERNAL LIBRARY 'ASSIMP' NOT FOUND")
-ENDIF (ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARY_DEBUG AND ASSIMP_LIBRARY_RELEASE AND ASSIMP_DLL_RELEASE AND ASSIMP_DLL_DEBUG)
-
-SET(ASSIMP_FOUND "NO")
-IF(ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARY_DEBUG AND ASSIMP_LIBRARY_RELEASE AND ASSIMP_DLL_RELEASE AND ASSIMP_DLL_DEBUG)
-	SET(ASSIMP_FOUND "YES")
-	SET(ASSIMP_LIBRARY debug ${ASSIMP_LIBRARY_DEBUG} optimized ${ASSIMP_LIBRARY_RELEASE})
-ENDIF(ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARY_DEBUG AND ASSIMP_LIBRARY_RELEASE AND ASSIMP_DLL_RELEASE AND ASSIMP_DLL_DEBUG)
-
-if(ASSIMP_DEBUG)
-	message(STATUS "assimp inc: ${ASSIMP_INCLUDE_DIRS}")
-	message(STATUS "assimp lib: ${ASSIMP_LIBRARY}")
-ENDIF(ASSIMP_DEBUG)
-
-if(AssImp_FIND_REQUIRED AND NOT (ASSIMP_LIBRARY AND ASSIMP_INCLUDE_DIRS))
-	message(FATAL_ERROR "Could not find assimp")
-ENDIF(AssImp_FIND_REQUIRED AND NOT (ASSIMP_LIBRARY AND ASSIMP_INCLUDE_DIRS))
-
-mark_as_advanced(ASSIMP_LIBRARY_DEBUG ASSIMP_LIBRARY_RELEASE ASSIMP_INCLUDE_DIRS)
+# Check if we found it!
+IF ( ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARIES )
+    SET( ASSIMP_FOUND TRUE )
+    MESSAGE(STATUS "Looking for ASSIMP - found")
+ELSE ( ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARIES )
+    SET( ASSIMP_FOUND FALSE )
+    MESSAGE(STATUS "Looking for ASSIMP - not found")
+ENDIF ( ASSIMP_INCLUDE_DIRS AND ASSIMP_LIBRARIES )
