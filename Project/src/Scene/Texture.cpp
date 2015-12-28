422e8b5709b1b4b5cb7d5fa2709e3048697b2d58
raphaelmenges <unrealer2@yahoo.de>
2015-12-28 21:13:02 +0100

    Fixed filtering of textures.

--- Project/src/Scene/Texture.cpp
+++ Project/src/Scene/Texture.cpp
@@ -48,7 +48,7 @@ Texture::Texture(std::string filepath)
 
     // Filtering
     glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
     // Unbind texture
