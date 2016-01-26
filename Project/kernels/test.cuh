99cbd5e80577ddabc2ac38edfde2782478a2a600
Nils Höhner <nils.hoehner@googlemail.com>
2016-01-26 18:47:01 +0100

    removed test kernel

--- Project/kernels/test.cuh
+++
@@ -1,6 +0,0 @@
-extern "C" // this is not necessary imho, but gives a better idea on where the function comes from
-{
-    void test();
-}
-
-
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,27 +1,14 @@
-extern "C" // this is not necessary imho, but gives a better idea on where the function comes from
-{
-    void test();    // test.cuh
-}
-
 #include "App.h"
 #include "Utilities/errorUtils.h"
-
-#include <iostream>
-#include <assimp/scene.h>
 
 // has to be included after opengl
 #include <cuda_gl_interop.h>
 
-
 // Main
 int main(void)
 {
-    std::cout << "hello from gcc" << std::endl;
-    test();
-
     // Init Cuda and enable OpenGL interop
     cudaErrorCheck(cudaGLSetGLDevice(0));
-
     App app;
 
     app.run();
