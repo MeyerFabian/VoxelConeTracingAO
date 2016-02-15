39766611b0b28085d523a6441eaf1d0a0ff4ff93
milan <miland@uni-koblenz.de>
2016-02-15 18:21:40 +0100

    added voxelcube class

---
+++ Project/src/VoxelVisualitation/Voxelcubes.cpp
@@ -0,0 +1,11 @@
+#include "Voxelcubes.h"
+
+VoxelCubes::VoxelCubes(FragmentList& fragmentList)
+{
+
+}
+
+void VoxelCubes::drawVoxel(FragmentList &fragmentList)
+{
+    fragmentList.bindReadonly();
+}
