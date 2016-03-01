c76d967eded5587ea39031ddefba06b3c8e87b66
raphaelmenges <unrealer2@yahoo.de>
2016-03-01 18:00:26 +0100

    Cleaning up Controllable. #11

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,7 +1,7 @@
 #include "App.h"
 #include "Utilities/errorUtils.h"
 
-// has to be included after opengl
+// Has to be included after opengl
 #include <cuda_gl_interop.h>
 
 // Main
@@ -10,6 +10,5 @@ int main(void)
     // Init Cuda and enable OpenGL interop
     cudaErrorCheck(cudaGLSetGLDevice(0));
     App app;
-
     app.run();
 }
