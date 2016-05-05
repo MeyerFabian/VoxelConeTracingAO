d956bb5673535e78fb5c306a1ebb3691b0bacb3d
MeyerFabian <fabian.meyer1337@web.de>
2016-05-05 17:03:24 +0200

    made windows build easier. more cmake commands to do work for you.

--- Project/cmake/FindGLFW3.cmake
+++ Project/cmake/FindGLFW3.cmake
@@ -8,8 +8,11 @@
 #
 
 SET(GLFW3_SEARCH_PATHS
+        $ENV{GLFW3}
         $ENV{GLFW3_ROOT}
         ${DEPENDENCIES_ROOT}
+	$ENV{PROGRAMFILES}/glfw/
+	C:/glfw
         /usr			# APPLE
         /usr/local		# APPLE
         /opt/local		# APPLE
@@ -45,17 +48,37 @@ ELSEIF (MSVC)
             DOC
             "The directory where GLFW/glfw3.h resides"
             )
+
+
+    IF(MSVC11)
+        set(SUFFIX "lib-vc2012")
+    ELSEIF(MSVC12)
+        set(SUFFIX "lib-vc2013")
+    ELSE()
+        set(SUFFIX "lib-vc2015")
+    ENDIF()
+
     FIND_LIBRARY(GLFW3_LIBRARY
             NAMES
-            glfw3
+                glfw3dll.lib
             PATHS
-            ${GLFW3_SEARCH_PATHS}
+                ${GLFW3_SEARCH_PATHS}
             PATH_SUFFIXES
-            lib
+                ${SUFFIX}
             DOC
-            "The directory where GLFW/glfw3.h resides"
+                "The directory where GLFW/glfw3dll.lib resides"
             )
 
+      FIND_PATH( GLFW_DLL
+            NAMES
+    						glfw3.dll
+            PATHS
+                ${GLFW3_SEARCH_PATHS}
+            PATH_SUFFIXES
+                ${SUFFIX}
+            DOC
+                "The glfw3.dll library."
+    	)
 ELSEIF(APPLE)
 
     FIND_PATH(GLFW3_INCLUDE_PATH
